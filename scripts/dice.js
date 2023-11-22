118b804e0dadf6a4f1b0a2c6ddd33c376577ced2
Zaitazu <andyschyns@outlook.com>
2023-11-22 21:23:03 +0100

    add base form for all players

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,7 +1,10 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
   dice3d.addSystem({ id: "Babelo", name: "Babelo"}, true);
   dice3d.addSystem({ id: "Red", name: "Red"}, true);
-  
+  dice3d.addSystem({ id: "Nasser", name: "Nasser"}, true);
+  dice3d.addSystem({ id: "Diokhan", name: "Diokhan"}, true);
+  dice3d.addSystem({ id: "Iteag", name: "Iteag"}, true);
+
   //Dice Babelo
   dice3d.addDicePreset({
     type: "d20",
@@ -58,6 +61,89 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
     system: "Red"
   });
 
+  //Dice Nasser
+  dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dices/images/Nasser20.png"
+    ],
+    system: "Nasser"
+  });
+
+  //Dice Diokhan
+  dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dices/images/Diokhan20.png"
+    ],
+    system: "Diokhan"
+  });
+
+  //Dice Iteag
+  dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dices/images/Iteag20.png"
+    ],
+    system: "Iteag"
+  });
 //  dice3d.addTexture("WikiRole", {
 //      name: "WikiRole",
 //      composite: "source-over",
