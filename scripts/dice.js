c3cf3a6f95ee94d32684f75168497e9523c2c2b9
Zaitazu <andyschyns@outlook.com>
2023-11-22 19:15:23 +0100

    update image link

--- scripts/dice.js
+++ scripts/dice.js
@@ -22,7 +22,7 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
       "17",
       "18",
       "19",
-      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dice/images/nat20.png"
+      "https://raw.githubusercontent.com/Zaitazu/ZaitaDices/main/images/nat20.png"
     ],
     bumpMaps: [
       ,
@@ -44,126 +44,10 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
       ,
       ,
       ,
-      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dice/images/nat20.png"
+      "https://raw.githubusercontent.com/Zaitazu/ZaitaDices/main/images/nat20_BUMP.png"
     ],
     system: "Zaita"
   },"d20");
-
-  dice3d.addDicePreset({
-    type: "d2",
-    labels: [
-      "†",
-      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dice/images/nat20.png"
-    ],
-    bumpMaps: [
-      ,
-      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dice/images/nat20.png"
-    ],
-    system: "Zaita"
-  },"d2");
- 
-  dice3d.addDicePreset({
-    type: "d4",
-    labels: [
-      "†",
-      "2",
-      "3",
-      "4"
-    ],
-    system: "Zaita"
-  },"d4");
- 
-  dice3d.addDicePreset({
-    type: "d6",
-    labels: [
-      "†",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6"
-    ],
-    system: "Zaita"
-  },"d6");
- 
-  dice3d.addDicePreset({
-    type: "df",
-    labels: [
-      "†",
-      "",
-      "☀"
-    ],
-    system: "Zaita",
-    fontScale: 1.3
-  },"df");
-    
-  dice3d.addDicePreset({
-    type: "d8",
-    labels: [
-      "†",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8"
-    ],
-    system: "Zaita"
-  },"d8");
-    
-  dice3d.addDicePreset({
-    type: "d10",
-    labels: [
-      "†",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10"
-    ],
-    system: "Zaita" 
-  },"d10");
- 
-  dice3d.addDicePreset({
-    type: "d12",
-    labels: [
-      "†",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "11",
-      "12"
-    ],
-    system: "Zaita"
-  },"d12");
-    
-  dice3d.addDicePreset({
-    type: "d100",
-    labels: [
-      "10",
-      "20",
-      "30",
-      "40",
-      "50",
-      "60",
-      "70",
-      "80",
-      "90",
-      "00",
-    ],
-    system: "Zaita"
-  },"d100");
 
 //  dice3d.addTexture("WikiRole", {
 //      name: "WikiRole",
