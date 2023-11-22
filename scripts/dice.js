ab090743e6e2e253c1769a751d63dfb01ea4621e
Zaitazu <andyschyns@outlook.com>
2023-11-22 15:41:11 +0100

    comit 1.0.2

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,58 +1,174 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
-    dice3d.addSystem({ id: "test", name: "test"}, true); 
-    dice3d.addDicePreset({
-      type: "d20",
-      labels: [
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "11",
-        "12",
-        "13",
-        "14",
-        "15",
-        "16",
-        "17",
-        "18",
-        "19",
-        "modules/ZaitaDices/images/nat20.png"
-      ],
-      bumpMaps: [
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        "modules/ZaitaDices/images/nat20_BUMP.png"
-      ],
-      system: "test"
-    },"d20");
+  dice3d.addSystem({ id: "Zaita", name: "Zaita"}, true); 
+  dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "modules/zaita_dice/images/nat20.png"
+    ],
+    bumpMaps: [
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      ,
+      "modules/zaita_dice/images/nat20_BUMP.png"
+    ],
+    system: "Zaita"
+  },"d20");
 
-  //  dice3d.addTexture("WikiRole", {
-  //      name: "WikiRole",
-  //      composite: "source-over",
-  //      source: "modules/arkangel/images/arkangel-texture.png",
-  //      bump:"modules/arkangel/images/arkangel-texture_bump.png" //puoi anche lasciarla vuoto
-  //  });
-  });
+  dice3d.addDicePreset({
+    type: "d2",
+    labels: [
+      "†",
+      "modules/zaita_dice/images/nat20.png"
+    ],
+    bumpMaps: [
+      ,
+      "modules/zaita_dice/images/nat20_BUMP.png"
+    ],
+    system: "Zaita"
+  },"d2");
+ 
+  dice3d.addDicePreset({
+    type: "d4",
+    labels: [
+      "†",
+      "2",
+      "3",
+      "4"
+    ],
+    system: "Zaita"
+  },"d4");
+ 
+  dice3d.addDicePreset({
+    type: "d6",
+    labels: [
+      "†",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6"
+    ],
+    system: "Zaita"
+  },"d6");
+ 
+  dice3d.addDicePreset({
+    type: "df",
+    labels: [
+      "†",
+      "",
+      "☀"
+    ],
+    system: "Zaita",
+    fontScale: 1.3
+  },"df");
+    
+  dice3d.addDicePreset({
+    type: "d8",
+    labels: [
+      "†",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8"
+    ],
+    system: "Zaita"
+  },"d8");
+    
+  dice3d.addDicePreset({
+    type: "d10",
+    labels: [
+      "†",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ],
+    system: "Zaita" 
+  },"d10");
+ 
+  dice3d.addDicePreset({
+    type: "d12",
+    labels: [
+      "†",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12"
+    ],
+    system: "Zaita"
+  },"d12");
+    
+  dice3d.addDicePreset({
+    type: "d100",
+    labels: [
+      "10",
+      "20",
+      "30",
+      "40",
+      "50",
+      "60",
+      "70",
+      "80",
+      "90",
+      "00",
+    ],
+    system: "Zaita"
+  },"d100");
+
+//  dice3d.addTexture("WikiRole", {
+//      name: "WikiRole",
+//      composite: "source-over",
+//      source: "modules/arkangel/images/arkangel-texture.png",
+//      bump:"modules/arkangel/images/arkangel-texture_bump.png" //puoi anche lasciarla vuoto
+//  });
+});
