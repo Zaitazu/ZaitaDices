28e556c299667cadd0cebea1589ab2e45ce350a9
Zaitazu <andyschyns@outlook.com>
2023-11-22 20:05:40 +0100

    add babelo crit

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,5 +1,8 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
-  dice3d.addSystem({ id: "Zaita", name: "Zaita"}, true); 
+  dice3d.addSystem({ id: "Zaita", name: "Zaita"}, true);
+  dice3d.addSystem({ id: "Babelo", name: "Babelo"}, true);
+  
+  //Dice Zaitazu
   dice3d.addDicePreset({
     type: "d20",
     labels: [
@@ -25,6 +28,34 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
       "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dices/images/teapot20.png"
     ],
     system: "Zaita"
+  });
+
+  //Dice Babelo
+  dice3d.addDicePreset({
+    type: "d20",
+    labels: [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+      "13",
+      "14",
+      "15",
+      "16",
+      "17",
+      "18",
+      "19",
+      "https://assets.forge-vtt.com/63767fb817b8033194fc400d/modules/zaita_dices/images/Bab20.png"
+    ],
+    system: "Babelo"
   });
 
 //  dice3d.addTexture("WikiRole", {
